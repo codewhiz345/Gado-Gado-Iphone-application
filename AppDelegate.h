d2955119464081aa40257df6b00a051c4a6e9b9f
codewhiz345 <sirandy37@gmail.com>
2016-03-11 12:14:39 +0530

    Added files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gado gado
+//
+//  Created by Pixlr IT on 23/02/16.
+//  Copyright © 2016 Pixlr IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
